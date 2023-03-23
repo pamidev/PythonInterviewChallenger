d886d8106b53a706a37b0b0d8df3ac0a97833fca
SzymonRebisz <SzymonRebiszgithub@users.noreply.github.com>
2023-03-23 20:29:24 +0100

    Added classes to statistics app

--- statistics/models.py
+++ statistics/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 
+class Experience(models.Model):
+    lvl = models.IntegerField(unique=True)
+
+class Answer_History(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete=models.DO_NOTHING)
+    question = models.ForeignKey('Polls.Questions', on_delete=models.DO_NOTHING )
