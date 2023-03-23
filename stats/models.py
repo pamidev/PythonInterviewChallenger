955784d93a3b146ea940e9d98e5fb227a2e87ec9
SzymonRebisz <SzymonRebiszgithub@users.noreply.github.com>
2023-03-23 21:07:07 +0100

    corrected spelling in stats\models

--- stats/models.py
+++ stats/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Experience(models.Model):
     lvl = models.IntegerField(unique=True)
 
-class Answer_History(models.Model):
+class AnswerHistory(models.Model):
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     question = models.ForeignKey('polls.Questions', on_delete=models.CASCADE)
