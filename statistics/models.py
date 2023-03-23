17f8036e97b4ca121ea405508da6815ff0b333ce
EDr918 <EDr918@users.noreply.github.com>
2023-03-23 20:05:58 +0100

    renamed class in statistics\ models.py

--- statistics/models.py
+++ statistics/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 
-class USER(models.MODEL):
+class User(models.MODEL):
     mail = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     is_creator = models.BooleanField(default=False)
     score = models.IntegerField(default=0)
-    experience = models.ForeignKey('STATISTICS.EXPERIENCE', on_delete=models.DO_NOTHING)
+    experience = models.ForeignKey('statistics.EXPERIENCE', on_delete=models.DO_NOTHING)
