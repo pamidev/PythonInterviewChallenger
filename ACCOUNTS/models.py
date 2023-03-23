a74c428804a1a9d2218147149b1916f825b9855c
SzymonRebisz <SzymonRebiszgithub@users.noreply.github.com>
2023-03-23 19:53:04 +0100

    corrected model

--- ACCOUNTS/models.py
+++ ACCOUNTS/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class USER(models.MODEL):
+    mail = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_creator = models.BooleanField(default=False)
+    score = models.IntegerField(default=0)
+    experience = models.ForeignKey('STATISTICS.EXPERIENCE', on_delete=models.DO_NOTHING)
