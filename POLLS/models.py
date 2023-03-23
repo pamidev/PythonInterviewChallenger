a4a39de798f288fd49a5c1e523319a4cf33e4bc2
SzymonRebisz <SzymonRebiszgithub@users.noreply.github.com>
2023-03-23 19:08:09 +0100

    added models to POLLS app

--- POLLS/models.py
+++ POLLS/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class QUESTIONS(models.Model):
+    question = models.CharField(max_length=255)
+    answer_1 = models.CharField(max_length=255)
+    answer_2 = models.CharField(max_length=255)
+    answer_3 = models.CharField(max_length=255)
+    answer_4 = models.CharField(max_length=255)
+    correct_answer = models.CharField(max_length=255)
+    is_verified = models.BooleanField(default=False)
+    experience = models.ForeignKey('STATISTICS.EXPERIENCE', on_delete=models.DO_NOTHING())
