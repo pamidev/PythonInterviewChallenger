1c539d1f57aa159c04084ffa747b73202c3f888d
Pawel Miduch <pamidev@duck.com>
2023-03-23 20:22:01 +0100

    Moved class User from statistic/models.py to accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    mail = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_creator = models.BooleanField(default=False)
+    score = models.IntegerField(default=0)
+    experience = models.ForeignKey('statistics.EXPERIENCE', on_delete=models.DO_NOTHING)
--- statistics/models.py
+++ statistics/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 
-class User(models.Model):
-    mail = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    is_creator = models.BooleanField(default=False)
-    score = models.IntegerField(default=0)
-    experience = models.ForeignKey('statistics.EXPERIENCE', on_delete=models.DO_NOTHING)
