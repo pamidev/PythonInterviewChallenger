427110c70a65c5ebbc8cb2885e78446343427008
SzymonRebisz <SzymonRebiszgithub@users.noreply.github.com>
2023-03-27 18:54:37 +0200

    Added admin user model

--- accounts/models.py
+++ accounts/models.py
@@ -9,3 +9,10 @@ class User(models.Model):
     is_creator = models.BooleanField(default=False)
     score = models.IntegerField(default=0)
     experience = models.ForeignKey('stats.Experience', on_delete=models.CASCADE)
+
+class Admin(models.Model):
+    mail = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=20)
