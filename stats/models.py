f35c63a297104d9caae6578af14c5e9b5e640618
Pawel Miduch <pamidev@duck.com>
2023-03-27 20:06:11 +0200

    Changed lvl field in Experience model from IntegerField to CharField

--- stats/models.py
+++ stats/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Experience(models.Model):
-    lvl = models.IntegerField(unique=True)
+    lvl = models.CharField(max_length=10, unique=True)
 
 
 class AnswerHistory(models.Model):
