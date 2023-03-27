ff3de97d311b46c2df0485f0136d35f45dbdcc79
SzymonRebisz <SzymonRebiszgithub@users.noreply.github.com>
2023-03-27 21:39:51 +0200

    Added views to log in

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid login credentials. Please try again.'
+    else:
+        error_message = ''
+    return render(request, 'login.html', {'error_message': error_message})
+
