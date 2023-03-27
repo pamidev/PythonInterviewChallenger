7b1c59f8c881f02d9fab1dbccb55dc488442a0fb
Pawel Miduch <pamidev@duck.com>
2023-03-27 21:31:45 +0200

    Created customized QuestionAdmin class and registered it in admin site

--- polls/admin.py
+++ polls/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from .models import Question
 
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        (None,               {'fields': ['answer_1']}),
+        (None,               {'fields': ['answer_2']}),
+        (None,               {'fields': ['answer_3']}),
+        (None,               {'fields': ['answer_4']}),
+        ('Correct answer',   {'fields': ['correct_answer']}),
+        ('Verified',         {'fields': ['is_verified']}),
+        ('Experience',       {'fields': ['experience']}),
+    ]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
+
+admin.site.register(Question, QuestionAdmin)
