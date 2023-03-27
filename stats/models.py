4174d1548c206ba07ef57ba418ed312f208ebcc2
Pawel Miduch <pamidev@duck.com>
2023-03-27 20:15:29 +0200

    Added __str__ method to Experience model and AnswerHistory model

--- stats/models.py
+++ stats/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Experience(models.Model):
     lvl = models.CharField(max_length=10, unique=True)
 
+    def __str__(self):
+        return self.lvl
+
 
 class AnswerHistory(models.Model):
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     question = models.ForeignKey('polls.Question', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
