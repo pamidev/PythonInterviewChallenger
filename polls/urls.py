3e151a51351d7631f64329f723653cd56c035599
Pawel Miduch <pamidev@duck.com>
2023-03-28 21:09:44 +0200

    Added new urls to polls.urls

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from . import views
+from .views import add_question, quiz
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', quiz, name='home'),
+    path('add_question/', add_question, name='add_question')
 ]
