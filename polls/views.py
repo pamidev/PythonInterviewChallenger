e74a9e6c22595a7cead877d96b1df198bbde8bfc
Pawel Miduch <pamidev@duck.com>
2023-03-28 21:08:59 +0200

    Created quiz and add_question views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,52 @@
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .forms import AddQuestionForm
+from .models import Question
 
 
-def index(request):
-    return HttpResponse("Hello adventurer. You're at the main page.")
+def quiz(request):
+    if request.method == 'POST':
+        questions = Question.objects.all()
+        score = 0
+        wrong = 0
+        correct = 0
+        total = 0
+        for q in questions:
+            total += 1
+            answer = request.POST.get(q.question)
+            items = vars(q)
+            print(items[answer])
+            if q.correct_answer == items[answer]:
+                score += 10
+                correct += 1
+            else:
+                wrong += 1
+        percent = score / (total * 10) * 100
+        context = {
+            'score': score,
+            'time': request.POST.get('timer'),
+            'correct': correct,
+            'wrong': wrong,
+            'percent': percent,
+            'total': total
+        }
+        return render(request, 'polls/results.html', context)
+    else:
+        questions = Question.objects.all()
+        context = {
+            'questions': questions
+        }
+        return render(request, 'polls/home.html', context)
+
+
+def add_question(request):
+    if request.user.is_staff:
+        form = AddQuestionForm()
+        if request.method == 'POST':
+            form = AddQuestionForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('/')
+        context = {'form': form}
+        return render(request, 'polls/add_question.html', context)
+    else:
+        return redirect('home')
