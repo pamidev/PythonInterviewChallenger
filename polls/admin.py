665725f653e88eb2454758734ac44d50d61b8068
Pawel Miduch <pamidev@duck.com>
2023-03-28 21:14:04 +0200

    Modified fields in QuestionAdmin class

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Question
 
 
@@ -11,11 +12,14 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['answer_3']}),
         (None,               {'fields': ['answer_4']}),
         ('Correct answer',   {'fields': ['correct_answer']}),
-        ('Verified',         {'fields': ['is_verified']}),
+        ('To verify',        {'fields': ['is_verified']}),
         ('Experience',       {'fields': ['experience']}),
     ]
-    list_display = ('question', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
+    list_display = ['id', 'question', 'correct_answer', 'pub_date', 'is_verified', 'experience',
+                    'was_published_recently']
+    list_display_links = ['id', 'question', 'pub_date']
+    list_filter = ['pub_date', 'is_verified']
+    list_per_page = 5
     search_fields = ['question']
 
 
