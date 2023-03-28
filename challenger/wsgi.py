fbbf89600a20acc8437266d9ca59810b376c2044
EDr918 <EDr918@users.noreply.github.com>
2023-03-28 18:01:43 +0200

    Added frontend

--- challenger/wsgi.py
+++ challenger/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for challenger project.
+WSGI config for polls project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'challenger.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polls.settings')
 
 application = get_wsgi_application()
