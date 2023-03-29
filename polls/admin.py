6ec5c88a65aa378d2449a1fd7dae761fcec5e9c2
Pawel Miduch <pamidev@duck.com>
2023-03-29 18:04:39 +0200

    Corrected fieldsets in QuestionAdmin class

--- polls/admin.py
+++ polls/admin.py
@@ -5,18 +5,17 @@ from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-        (None,               {'fields': ['answer_1']}),
-        (None,               {'fields': ['answer_2']}),
-        (None,               {'fields': ['answer_3']}),
-        (None,               {'fields': ['answer_4']}),
-        ('Correct answer',   {'fields': ['correct_answer']}),
-        ('To verify',        {'fields': ['is_verified']}),
-        ('Experience',       {'fields': ['experience']}),
+        (None, {'fields': ['question']}),
+        (None, {'fields': ['pub_date']}),
+        (None, {'fields': ['answer_1']}),
+        (None, {'fields': ['answer_2']}),
+        (None, {'fields': ['answer_3']}),
+        (None, {'fields': ['answer_4']}),
+        (None, {'fields': ['correct_answer']}),
+        (None, {'fields': ['is_verified']}),
+        (None, {'fields': ['experience']}),
     ]
-    list_display = ['id', 'question', 'correct_answer', 'pub_date', 'is_verified', 'experience',
-                    'was_published_recently']
+    list_display = ['id', 'question', 'correct_answer', 'pub_date', 'is_verified', 'experience']
     list_display_links = ['id', 'question', 'pub_date']
     list_filter = ['pub_date', 'is_verified']
     list_per_page = 5
