083258992b41628c9c1826b5f2772a29182f9703
SzymonRebisz <SzymonRebiszgithub@users.noreply.github.com>
2023-03-29 18:27:33 +0200

    Corected urlpattern

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import CustomLoginView
-from polls.views import index
+from accounts.views import profile
 
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('accounts/profile/', index, name='profil'),
+    path('accounts/profile/', profile, name='profil'),
 ]
