d478c6505749b3717197437b65e76ae5772c2add
Pawel Miduch <pamidev@duck.com>
2023-03-30 20:42:11 +0200

    Added field experience to class AddQuestionForm

--- polls/forms.py
+++ polls/forms.py
@@ -5,4 +5,4 @@ from .models import *
 class AddQuestionForm(ModelForm):
     class Meta:
         model = Question
-        fields = ['question', 'answer_1', 'answer_2', 'answer_3', 'answer_4', 'correct_answer']
+        fields = ['question', 'answer_1', 'answer_2', 'answer_3', 'answer_4', 'correct_answer', 'experience']
