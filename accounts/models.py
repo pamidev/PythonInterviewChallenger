057a7cf691154832d236a55d76c7ddade98ded97
Pawel Miduch <pamidev@duck.com>
2023-04-13 14:54:22 +0200

    Added an empty line between the classes

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,7 @@ class User(models.Model):
     score = models.IntegerField(default=0)
     experience = models.ForeignKey('stats.Experience', on_delete=models.CASCADE)
 
+
 class Admin(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
