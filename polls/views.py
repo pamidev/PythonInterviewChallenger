5ef0fef2e7a1675bcf72af5403abb01ce74e34da
Pawel Miduch <pamidev@duck.com>
2023-04-17 21:45:22 +0200

    Trial of 3 random questions and their results

--- polls/views.py
+++ polls/views.py
@@ -1,44 +1,66 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from .forms import AddQuestionForm
 from .models import Question
 
 
+def questions_all():
+    questions = Question.objects.filter(is_verified=True)
+    return questions
+
+
+def questions_3():
+    questions = Question.objects.filter(is_verified=True).order_by('?')[:3]
+    return questions
+
+
+def results(request):
+    if request.user.is_authenticated:
+        questions = questions_all()
+    else:
+        questions = questions_3()
+
+    score = 0
+    wrong = 0
+    correct = 0
+    total = 0
+
+    for q in questions:
+        total += 1
+        answer = request.POST.get(q.question)
+        items = vars(q)
+        print(items[answer])
+        if q.correct_answer == items[answer]:
+            score += 10
+            correct += 1
+        else:
+            wrong += 1
+
+    percent = score / (total * 10) * 100
+    context = {
+        'score': score,
+        'percent': percent,
+        'total': total,
+        'correct': correct,
+        'wrong': wrong,
+    }
+    return context
+
+
 def quiz(request):
     if request.method == 'POST':
-        questions = Question.objects.filter(is_verified=True)
-        score = 0
-        wrong = 0
-        correct = 0
-        total = 0
-        for q in questions:
-            total += 1
-            answer = request.POST.get(q.question)
-            items = vars(q)
-            print(items[answer])
-            if q.correct_answer == items[answer]:
-                score += 10
-                correct += 1
-            else:
-                wrong += 1
-        percent = score / (total * 10) * 100
-        context = {
-            'score': score,
-            'percent': percent,
-            'total': total,
-            'correct': correct,
-            'wrong': wrong,
-        }
+        context = results(request)
         return render(request, 'polls/results.html', context)
     else:
-        questions = Question.objects.filter(is_verified=True)
+        if request.user.is_authenticated:
+            questions = questions_all()
+        else:
+            questions = questions_3
         context = {
             'questions': questions
         }
         return render(request, 'polls/quiz.html', context)
 
 
-@login_required(login_url='home')
 def add_question(request):
     if request.user.is_authenticated:
         form = AddQuestionForm()
