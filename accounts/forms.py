8f0cafa2e2626ed942d92d701bc82a406f82baa1
SzymonRebisz <SzymonRebiszgithub@users.noreply.github.com>
2023-04-18 22:46:15 +0200

    Added registration and log in walidation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -6,3 +7,9 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2', 'first_name', 'last_name']
+
+    # def clean_username(self):
+    #     username = self.cleaned_data['username']
+    #     if User.objects.filter(username=username).exists():
+    #         raise forms.ValidationError("This username is already taken.")
+    #     return username
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from .forms import CreateUserForm
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from .forms import CreateUserForm
 
 
 def registerPage(request):
@@ -13,11 +13,14 @@ def registerPage(request):
         if request.method == 'POST':
             form = CreateUserForm(request.POST)
             if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + user)
-
-                return redirect('user_login')
+                username = form.cleaned_data.get('username')
+                if User.objects.filter(username=username).exists():
+                    messages.error(request, 'Username is already taken.')
+                else:
+                    form.save()
+                    user = form.cleaned_data.get('username')
+                    messages.success(request, 'Account was created for ' + user)
+                    return redirect('user_login')
 
         context = {'form': form}
         return render(request, 'accounts/register.html', context)
@@ -30,15 +33,12 @@ def loginPage(request):
         if request.method == 'POST':
             username = request.POST.get('username')
             password = request.POST.get('password')
-
             user = authenticate(request, username=username, password=password)
-
             if user is not None:
                 login(request, user)
                 return redirect('home')
             else:
                 messages.info(request, 'Username or password is incorrect')
-
         context = {}
         return render(request, 'accounts/login.html', context)
 
