15ef1c333a3749c97d55e38495871918026659e9
Pawel Miduch <pamidev@duck.com>
2023-05-14 19:32:37 +0200

    Fixed DATABASES

--- challenger/settings.py
+++ challenger/settings.py
@@ -74,9 +74,9 @@ WSGI_APPLICATION = 'challenger.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
